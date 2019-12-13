31481b1c03019d2f3cfb8fccc90e8738fc242ef9
RobinSanta <robinsntn@gmail.com>
2019-12-13 15:00:09 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 let addToy = false
+let uniqueCard
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  const toyView = document.querySelector('#toy-collection')
+  
+  fetch('http://localhost:3000/toys').then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    createToyCard(json)
+  })
+  
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +21,87 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
+  
+  toyForm.addEventListener('submit', (event) => {
+    const  newToyName = event.target.name.value
+    const  newToyUrl = event.target.image.value
+    event.preventDefault()
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name: newToyName,
+        image: newToyUrl,
+        likes: 0
+      })
+    }).then(function(response) {
+      return response.json()
+    }).then(function(data) {
+      console.log(data)
+      createOneCard(data)
+    })
+  })
+
+  document.addEventListener("click", function(event) {
+    // console.log(event.path[1].querySelector('p').textContent.split(" ")[0])
+    let currentLikes = parseInt(event.path[1].querySelector('p').textContent.split(" ")[0]) + 1
+    let currentItem = event.target
+    if (event.target.classList == 'like-btn') {
+    fetch(`http://localhost:3000/toys/${currentItem.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        likes: currentLikes
+      })
+    })
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      console.log(data)
+      console.log(currentItem.parentNode.children[2])
+      currentItem.parentNode.children[2].textContent = `${currentLikes} likes`
+      // currentItem.likes.textContent = `${data.likes} likes`
+    })
+  }
+  })
 
 })
+
+function createToyCard(json) {
+  const toyView = document.querySelector('#toy-collection')
+  json.forEach(toy => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    uniqueCard = toyView.appendChild(div)
+    addInfoToCard(toy)
+  })
+}
+
+function createOneCard(json) {
+  const toyView = document.querySelector('#toy-collection')
+  const div = document.createElement('div')
+  div.classList.add('card')
+  uniqueCard = toyView.appendChild(div)
+  addInfoToCard(json)
+}
+
+function addInfoToCard(toy) {
+  const h2 = document.createElement('h2')
+  const img = document.createElement('img')
+  img.classList.add('toy-avatar')
+  const p = document.createElement('p')
+  const button = document.createElement('button')
+  button.id = `${toy.id}`
+  uniqueCard.appendChild(h2).textContent = toy.name
+  uniqueCard.appendChild(img).src = toy.image
+  uniqueCard.appendChild(p).textContent = `${toy.likes} Likes`
+  uniqueCard.appendChild(button).textContent = `Like`
+  button.classList.add('like-btn')
+}
